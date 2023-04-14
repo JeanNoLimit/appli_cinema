5397e310a3520666792a6bba9aac7b096e4b2989
Jean-Noel Richert <jean.noel.richert@gmail.com>
2023-04-14 16:55:37 +0200

    Mise à jour bd

--- bdCinema.sql
+++ bdCinema.sql
@@ -3,7 +3,182 @@
 -- Version du serveur:           8.0.30 - MySQL Community Server - GPL
 -- SE du serveur:                Win64
 -- HeidiSQL Version:             12.1.0.6537
+-- ---------------------------------------------------------- --------------------------------------------------------
+-- Hôte :                        127.0.0.1
+-- Version du serveur:           5.7.24 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Listage de la structure de la base pour cinema_jn
+CREATE DATABASE IF NOT EXISTS `cinema_jn` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `cinema_jn`;
+
+-- Listage de la structure de la table cinema_jn. acteur
+CREATE TABLE IF NOT EXISTS `acteur` (
+  `id_acteur` int(11) NOT NULL AUTO_INCREMENT,
+  `id_personne` int(11) NOT NULL,
+  PRIMARY KEY (`id_acteur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `FK_acteur_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.acteur : ~4 rows (environ)
+/*!40000 ALTER TABLE `acteur` DISABLE KEYS */;
+INSERT INTO `acteur` (`id_acteur`, `id_personne`) VALUES
+	(1, 2),
+	(2, 3),
+	(3, 4),
+	(4, 6);
+/*!40000 ALTER TABLE `acteur` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. film
+CREATE TABLE IF NOT EXISTS `film` (
+  `id_film` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(100) COLLATE utf8_bin NOT NULL,
+  `date_sortie` date NOT NULL,
+  `synopsis` text COLLATE utf8_bin,
+  `duree` int(11) NOT NULL,
+  `affiche` varchar(50) COLLATE utf8_bin DEFAULT NULL,
+  `note` int(11) DEFAULT NULL,
+  `id_realisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`),
+  KEY `FK_film_realisateur` (`id_realisateur`),
+  CONSTRAINT `FK_film_realisateur` FOREIGN KEY (`id_realisateur`) REFERENCES `realisateur` (`id_realisateur`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.film : ~2 rows (environ)
+/*!40000 ALTER TABLE `film` DISABLE KEYS */;
+INSERT INTO `film` (`id_film`, `titre`, `date_sortie`, `synopsis`, `duree`, `affiche`, `note`, `id_realisateur`) VALUES
+	(1, 'Goldfinger', '1965-02-18', NULL, 112, NULL, NULL, 1),
+	(2, 'LE SEIGNEUR DES ANNEAUX : LA COMMUNAUTÉ DE L\'ANNEAU', '2001-12-19', NULL, 178, NULL, NULL, 3);
+/*!40000 ALTER TABLE `film` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. genre
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id_genre` int(11) NOT NULL,
+  `libelle_genre` varchar(50) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.genre : ~13 rows (environ)
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+INSERT INTO `genre` (`id_genre`, `libelle_genre`) VALUES
+	(1, 'Action'),
+	(2, 'Aventure'),
+	(3, 'Biopic'),
+	(4, 'Comédie'),
+	(5, 'Drame'),
+	(6, 'Famille'),
+	(7, 'Guerre'),
+	(8, 'Historique'),
+	(9, 'Policier'),
+	(10, 'Romance'),
+	(11, 'Science-Fiction'),
+	(12, 'thriller'),
+	(13, 'Western');
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. jouer
+CREATE TABLE IF NOT EXISTS `jouer` (
+  `id_film` int(11) NOT NULL,
+  `id_acteur` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_acteur`,`id_role`),
+  KEY `FK_jouer_role` (`id_role`),
+  KEY `FK_jouer_acteur` (`id_acteur`),
+  CONSTRAINT `FK_jouer_acteur` FOREIGN KEY (`id_acteur`) REFERENCES `acteur` (`id_acteur`),
+  CONSTRAINT `FK_jouer_film` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `FK_jouer_role` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.jouer : ~4 rows (environ)
+/*!40000 ALTER TABLE `jouer` DISABLE KEYS */;
+INSERT INTO `jouer` (`id_film`, `id_acteur`, `id_role`) VALUES
+	(1, 1, 1),
+	(1, 2, 2),
+	(1, 3, 3),
+	(2, 4, 4);
+/*!40000 ALTER TABLE `jouer` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. personne
+CREATE TABLE IF NOT EXISTS `personne` (
+  `id_personne` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(50) COLLATE utf8_bin NOT NULL,
+  `date_naissance` date NOT NULL,
+  `sexe` varchar(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.personne : ~6 rows (environ)
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` (`id_personne`, `nom`, `prenom`, `date_naissance`, `sexe`) VALUES
+	(1, 'Hamilton', 'Guy', '1922-09-11', 'M'),
+	(2, 'Connery', 'Sean', '1930-08-25', 'M'),
+	(3, 'Blackman', 'Honor', '1925-08-22', 'F'),
+	(4, 'Fröbe', 'Gert', '1913-09-25', 'M'),
+	(5, 'Jackson', 'Peter', '1961-10-31', 'M'),
+	(6, 'WOOD', 'ELIJAH', '1981-01-28', 'M');
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. posseder
+CREATE TABLE IF NOT EXISTS `posseder` (
+  `id_film` int(11) NOT NULL,
+  `id_genre` int(11) NOT NULL,
+  PRIMARY KEY (`id_film`,`id_genre`),
+  KEY `id_genre` (`id_genre`),
+  CONSTRAINT `FK_posseder_film` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`),
+  CONSTRAINT `posseder_ibfk_2` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id_genre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.posseder : ~0 rows (environ)
+/*!40000 ALTER TABLE `posseder` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posseder` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. realisateur
+CREATE TABLE IF NOT EXISTS `realisateur` (
+  `id_realisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `id_personne` int(11) NOT NULL,
+  PRIMARY KEY (`id_realisateur`),
+  UNIQUE KEY `id_personne` (`id_personne`),
+  CONSTRAINT `FK_realisateur_personne` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id_personne`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.realisateur : ~2 rows (environ)
+/*!40000 ALTER TABLE `realisateur` DISABLE KEYS */;
+INSERT INTO `realisateur` (`id_realisateur`, `id_personne`) VALUES
+	(1, 1),
+	(3, 5);
+/*!40000 ALTER TABLE `realisateur` ENABLE KEYS */;
+
+-- Listage de la structure de la table cinema_jn. role
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_role` varchar(50) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Listage des données de la table cinema_jn.role : ~4 rows (environ)
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` (`id_role`, `nom_role`) VALUES
+	(1, 'James Bond'),
+	(2, 'Auric Goldfinger'),
+	(3, 'Pussy Galore'),
+	(4, 'Frodon Sacquet');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
