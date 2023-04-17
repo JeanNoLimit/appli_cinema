34ac48242ec3eb1aeb09607e805d7467e34085ac
Jean-Noel <jean.noel.richert@gmail.com>
2023-04-17 15:21:01 +0200

    création du menu de navigation

---
+++ www/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div id="wrap">
+                <ul class="navbar">
+                    <li>
+                        <a href="#">Acceuil</a>
+                    </li>
+                    <li>
+                        <a href="#">Filtrer affichages</a>
+                        <ul>
+                            <li><a href="#">Par genres</a></li>
+                            <li><a href="#">Par réalisateurs</a></li>
+                            <li><a href="#">Par acteurs</a></li>
+                            <li><a href="#">Par rôle</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Formulaires</a>
+                        <ul>
+                            <li><a href="#">Nouveau Réalisateur/acteur</a></li>
+                            <li><a href="#">Nouveau film</a></li>
+                            <li><a href="#">Nouveau rôle</a></li>
+                            <li><a href="#">Nouveau casting</a></li>
+                        </ul>
+                    </li>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
