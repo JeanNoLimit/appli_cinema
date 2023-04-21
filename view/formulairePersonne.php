faed681c47e0fee4db537909162a3a7738a4203f
Jean-Noël Richert <jean.noel.richert@gmail.com>
2023-04-21 15:11:59 +0200

    Création du formulaire Réal/Acteur

--- view/formulairePersonne.php
+++ view/formulairePersonne.php
@@ -1,5 +1,53 @@
 <?php ob_start()?>
 
+<div class="formulaire">
+    <h3>Formulaire de saisie : <br> Nouveau réalisateur/acteur </h3>
+    <form action="#" method="post">
+        <div>
+            <label for="nom">Nom : 
+                <input type="text" name="nom" id="nom" maxlength="25">
+            </label>
+        </div>
+        <div>
+            <label for="prenom">Prénom :
+                <input type="text" name="nom" id="nom" maxlength="25">
+            </label>
+        </div>
+        <div>
+            <label for="date_naissance">Date de naissance :
+                <input type="date" name="date_naissance" id="date_naissance" min="1862-10-19">
+            </label>
+        </div>
+        <div>
+            <p>Sexe : </p>
+            <input type="radio" name="sexe" id="homme" value="M">
+                <label for="homme">homme</label>
+            <input type="radio" name="sexe" id="femme" value="F">
+                <label for="femme">femme</label>
+        </div>
+        
+        
+        <div>
+            <p>Métier(s) : </p>
+            <input type="checkbox" name="metier" id="realisateur">
+                <label for="realisateur">réalisateur</label>
+            <input type="checkbox" name="metier" id="acteur">
+                <label for="acteur">acteur</label>
+        </div>
+        
+        <button type="submit" name="submit">Envoyer</button>
+    </form>
+</div>
+
+
+
+
+
+
+
+
+
+
 
 
 
