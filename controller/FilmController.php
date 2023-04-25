13c3880f410fe94b07ffccbab111c4b8e29f295d
Jean-Noël Richert <jean.noel.richert@gmail.com>
2023-04-25 16:04:56 +0200

    Ajout messages d'alertes dans FilmController

--- controller/FilmController.php
+++ controller/FilmController.php
@@ -75,8 +75,11 @@ class FilmController {
     //*********VUE FORMULAIRE FILM*********//
 
     public function formulaireFilm(){
+
+         // **************************REQUETES POUR L'AFFICHAGE DU FORMULAIRE****************************//
+
         $pdo = Connect::seConnecter();
-        // On aura besoin d'uploader une image. Pour ce faire suivre le tuto : https://espritweb.fr/comment-uploader-une-image-en-php/
+       
         $requete_genre = $pdo->query('SELECT g.id_genre AS id, libelle_genre
                                         FROM  genre g
                                         ORDER BY libelle_genre');
@@ -85,8 +88,53 @@ class FilmController {
                                         INNER JOIN realisateur r ON p.id_personne=r.id_personne
                                         ORDER BY nom, prenom;');
 
+
+
+         // **************************TRAITEMENT DES DONNEES RECEPTIONNEES DANS LE FORMULAIRE****************************//
+
         if(isset($_POST['submitFilm'])){
-           
+
+            // **************************VERIFICATIONS FICHIER IMAGE****************************//
+
+             // Pour uploader une image, suivre le tuto : https://espritweb.fr/comment-uploader-une-image-en-php/
+             // 1. On récupère les données de l'image
+            if(isset($_FILES['affiche'])){
+                $tmpName = $_FILES['affiche']['tmp_name'];
+                $name = $_FILES['affiche']['name'];
+                $size = $_FILES['affiche']['size'];
+                $error = $_FILES['affiche']['error'];
+            }
+
+            
+            // 2.On va vérifier l'extension du fichier pour n'accepter que les images
+            //      1. On découpe le nom du fichier au niveau du point . -> Renvoie le résultat dans un tableau $tabExtension
+            //      2. On récupère le dernier élément du tableau. strtolower permet de mettre en minuscule la chaine de caractère
+            $tabExtension = explode('.', $name);
+            $extension = strtolower(end($tabExtension));
+
+            // On liste les extensions autorisé
+            $extensions= ['jpg', 'png', 'jpeg', 'gif'];
+            //Taille max que l'on accepte (en bytes) 1Mo;
+            $tMax= 1048576;
+
+
+            if(in_array($extension, $extensions) && $size<=$tMax && $error==0 ) {
+
+                // uniqid() est une fonction qui permet de générer un nom unique. 2 arguments : La chaine de caractère qui sert de préfixe et un booléen pour augmenter la taille de chaîne pour + de sécurité
+                $uniqueName =uniqid('', true);
+                $file=$uniqueName.".".$extension;
+
+                // Fonction qui permet de déplacer le fichier dans le bon dossier. https://www.php.net/manual/fr/function.move-uploaded-file.php
+                move_uploaded_file($tmpName, './upload/'. $file);
+            }else {
+                echo "Mauvaise extension ou image trop importante ou image absente.";
+                $file=null;
+            }
+
+
+
+            // **************************FILTRAGE DES DONNEES********************************//
+
             $titre=filter_input(INPUT_POST, "titre", FILTER_SANITIZE_SPECIAL_CHARS);
             // On obtient un tableau qui contient les id_genres
             $id_genres=filter_input(INPUT_POST, "id_genres", FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
@@ -107,57 +155,35 @@ class FilmController {
                 $note=null;
             }
 
-            // On récupère les données de l'image
-            if(isset($_FILES['affiche'])){
-                $tmpName = $_FILES['affiche']['tmp_name'];
-                $name = $_FILES['affiche']['name'];
-                $size = $_FILES['affiche']['size'];
-                $error = $_FILES['affiche']['error'];
-            }
-            // ***********VERIFICATIONS FICHIER IMAGE*********************//
-            //On va vérifier l'extension du fichier pour n'accepter que les images
-            //      1. On découpe le nom du fichier au niveau du point . -> Renvoie le résultat dans un tableau $tabExtension
-            //      2. On récupère le dernier élément du tableau. strtolower permet de mettre en minuscule la chaine de caractère
-            $tabExtension = explode('.', $name);
-            $extension = strtolower(end($tabExtension));
-            // On liste les extensions autorisé
-            $extensions= ['jpg', 'png', 'jpeg', 'gif'];
-            //Taille max que l'on accepte (en bytes) 1Mo;
-            $tMax= 1048576;
-
-
-            if(in_array($extension, $extensions) && $size<=$tMax && $error==0) {
-
-                // uniqid() est une fonction qui permet de générer un nom unique. 2 arguments : La chaine de caractère qui sert de préfixe et un booléen pour augmenter la taille de chaîne pour + de sécurité
-                $uniqueName =uniqid('', true);
-                $file=$uniqueName.".".$extension;
-
-                // Fonction qui permet de déplacer le fichier dans le bon dossier. https://www.php.net/manual/fr/function.move-uploaded-file.php
-                move_uploaded_file($tmpName, './upload/'. $file);
-            }else {
-                echo "Mauvaise extension ou image trop importante";
-            }
-
 
             // Si les filtres ont renvoyé un résultat dans les variables....
             if($titre && $id_genres && $date_sortie && $duree && $id_realisateur){
-               
-                $pdo= Connect::seConnecter();
-                // Requête pour insérer les données dans la table film
-                $requete_film = $pdo ->prepare('INSERT INTO film(titre, date_sortie, duree, note, id_realisateur, affiche, synopsis)
-                                                     VALUES (:titre, :date_sortie, :duree, :note, :id_realisateur, :affiche, :synopsis)');
-                $requete_film->execute(['titre' => $titre, 'date_sortie' => $date_sortie, 'duree' => $duree, 'note' => $note,'id_realisateur' => $id_realisateur, 'affiche' => $file, 'synopsis' => $synopsis]);
+               if(strlen($titre)>100){
+                    $_SESSION['messageAlert']="Le titre ne doit pas dépasser 100 caractères.";
+               }elseif(count($id_genres)>3){
+                    $_SESSION['messageAlert']="Veuillez rentrer 3 genres au maximum.";
+               }elseif( date("Y-m-d")<$date_sortie){
+                    $_SESSION['messageAlert']="Veuillez rentrer une date valide.";
+               }elseif($duree>1265 || $duree<=0){
+                    $_SESSION['messageAlert']="Veuillez rentrer une durée valide.";
+               }else{
+                    $pdo= Connect::seConnecter();
+                    // Requête pour insérer les données dans la table film
+                    $requete_film = $pdo ->prepare('INSERT INTO film(titre, date_sortie, duree, note, id_realisateur, affiche, synopsis)
+                                                        VALUES (:titre, :date_sortie, :duree, :note, :id_realisateur, :affiche, :synopsis)');
+                    $requete_film->execute(['titre' => $titre, 'date_sortie' => $date_sortie, 'duree' => $duree, 'note' => $note,'id_realisateur' => $id_realisateur, 'affiche' => $file, 'synopsis' => $synopsis]);
 
-                // On récupère l'identifiant du film créée
-                $film_id = $pdo->lastInsertId();
+                    // On récupère l'identifiant du film créée
+                    $film_id = $pdo->lastInsertId();
 
-                // Requête pour insérer les données dans la table posséder qui lie la table genre à film
-                foreach($id_genres as $genre){
-                    $req_genre =$pdo -> prepare('INSERT INTO posseder (id_genre, id_film)
-                                                    VALUES ( :id_genre, :id_film) ');
-                    $req_genre ->execute (['id_genre'=> $genre, 'id_film' => $film_id]);
-
-                }
+                    // Requête pour insérer les données dans la table posséder qui lie la table genre à film
+                    foreach($id_genres as $genre){
+                        $req_genre =$pdo -> prepare('INSERT INTO posseder (id_genre, id_film)
+                                                        VALUES ( :id_genre, :id_film) ');
+                        $req_genre ->execute (['id_genre'=> $genre, 'id_film' => $film_id]);
+                    }
+                    $_SESSION['messageSucces']="Film enregistré avec succès.";
+               }
             }    
         }
         require "view/formulaireFilm.php";
